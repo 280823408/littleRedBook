59455af9ed7859ac70393e8b01f1b2da8055070b
Mike <280823408@qq.com>
2025-02-21 09:03:08 +0800

    新建Spring项目

---
+++ src/main/java/com/example/littleredbook/LittleRedBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.littleredbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleRedBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LittleRedBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/littleredbook/LittleRedBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.littleredbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleRedBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
